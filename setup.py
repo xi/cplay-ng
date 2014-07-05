dc03fa18b1a6289aeaccef51f9dd0b444aa1d5cf
Tobias Bengfort <tobias.bengfort@gmx.net>
2014-07-05 10:59:32 +0200

    extract version and name in setup.py from cplay.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ try:
 except:
     babel = None
 
+
 class _build(build):
     sub_commands = ([('compile_catalog', None)] + build.sub_commands
         if babel is not None
@@ -22,9 +23,15 @@ class _install_lib(install_lib):
         install_lib.run(self)
 
 
+with open('cplay/cplay.py') as fh:
+    for line in fh:
+        if line.startswith('__version__'):
+            name, version = line.split('"')[1].split()
+            break
+
 setup(
-    name='cplay-ng',
-    version='2.0.1',
+    name=name,
+    version=version,
     description="A curses front-end for various audio players",
     long_description=open('README.rst').read(),
     url='https://github.com/xi/cplay-ng',
