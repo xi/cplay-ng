1327b5364e15d2d530ac5dbc3b742b89b7a07097
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-01-30 22:45:07 +0100

    bump version to 2.2.0

--- cplay/cplay.py
+++ cplay/cplay.py
@@ -20,7 +20,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 """
 
-__version__ = 'cplay-ng 2.1.2'
+__version__ = 'cplay-ng 2.2.0'
 
 import os
 import re
