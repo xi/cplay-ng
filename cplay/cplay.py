f62b054625b6f06d4a2d63312b39cb73e8111876
Tobias Bengfort <tobias.bengfort@posteo.de>
2017-06-05 13:48:12 +0200

    bump version to 2.3.0

--- cplay/cplay.py
+++ cplay/cplay.py
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 from __future__ import unicode_literals
 
-__version__ = 'cplay-ng 2.2.0'
+__version__ = 'cplay-ng 2.3.0'
 
 import os
 import re
